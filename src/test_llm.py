02d0751f5bbdba944681ee754745ba44de3c525d
Adarsh Maurya <adarshmaurya7@gmail.com>
2025-09-04 03:01:45 +0530

    fixed test_llm

--- src/test_llm.py
+++ src/test_llm.py
@@ -5,7 +5,7 @@ class ResponseType(BaseModel):
     output: str
 
 if __name__ == '__main__':
-    llm = LLMService(api_key="AIzaSyAMMWwaKUUHJJVWeSIrtd4-z4Uj6L_fKYs")
+    llm = LLMService(api_key="")
 
     response = llm.ask(
         user_query="tell me a joke",
